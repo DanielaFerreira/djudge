d45835c21e5eff23b7b8bc702210cf452894d425
eddunic <eduardo.bitencourt007@gmail.com>
2019-06-03 00:47:27 -0400

    Submissão e compilação

--- src/main/java/servlet/SubmissionServlet.java
+++ src/main/java/servlet/SubmissionServlet.java
@@ -1,17 +1,36 @@
 package servlet;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import static org.apache.http.HttpHeaders.USER_AGENT;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 
 /**
  *
@@ -19,107 +38,153 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
  */
 public class SubmissionServlet extends HttpServlet {
 
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//	    throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
 
-//	String source = request.getParameter("source");
-//	String compiler = request.getParameter("compilerId");
-//
-//	PrintWriter out = response.getWriter();
-//	out.println(source + " " + compiler);
+	doPost(request, response);
 
-    //	boolean isMultiPart = FileUpload.isMultipartContent(request);
-    //
-    //	if (isMultiPart) {
-    //
-    //	    FileItemFactory factory = new DiskFileItemFactory();
-    //	    ServletFileUpload upload = new ServletFileUpload(factory);
-    //	    String formulario = "";
-    //	    try {
-    //		List items = upload.parseRequest(request);
-    //		Iterator iter = items.iterator();
-    //		while (iter.hasNext()) {
-    //		    FileItem item = (FileItem) iter.next();
-    //		    if (item.getFieldName().equals("tipoForm")) {
-    //			formulario = item.getString();
-    //			out.println(formulario);
-    //		    }
-    //		}
-    //	    } catch (FileUploadException ex) {
-    //		ex.printStackTrace();
-    //	    } catch (Exception ex) {
-    //		ex.printStackTrace();
-    //	    }
-    //	}
-    //}
-    
-     private boolean isMultipart;
-   private String filePath;
-   private int maxFileSize = 50 * 1024;
-   private int maxMemSize = 4 * 1024;
-   private File file ;
+    }
 
-   public void init( ){
-      // Get the file location where it would be stored.
-      filePath = getServletContext().getInitParameter("file-upload"); 
-   }
-   
-   public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, java.io.IOException {
-   
-      // Check that we have a file upload request
-      isMultipart = ServletFileUpload.isMultipartContent(request);
-      response.setContentType("text/html");
-      java.io.PrintWriter out = response.getWriter( );
-   
-      if( !isMultipart ) {
-         out.println("<html>");
-         out.println("<head>");
-         out.println("<title>Servlet upload</title>");  
-         out.println("</head>");
-         out.println("<body>");
-         out.println("<p>No file uploaded</p>"); 
-         out.println("</body>");
-         out.println("</html>");
-         return;
-      }
-  
-      DiskFileItemFactory factory = new DiskFileItemFactory();
-   
-      // maximum size that will be stored in memory
-      factory.setSizeThreshold(maxMemSize);
-   
-      // Location to save data that is larger than maxMemSize.
-      factory.setRepository(new File("/home/eduardo"));
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
 
-      // Create a new file upload handler
-      ServletFileUpload upload = new ServletFileUpload(factory);
-   
-      // maximum file size to be uploaded.
-      upload.setSizeMax( maxFileSize );
+// salva source + compilerID
+//	ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
+//	List<FileItem> multifiles = null;
+//	try {
+//	    multifiles = sf.parseRequest(request);
+//	} catch (FileUploadException ex) {
+//	    Logger.getLogger(SubmissionServlet.class.getName()).log(Level.SEVERE, null, ex);
+//	}
+//	
+//	for(FileItem item : multifiles){
+//	    try {
+//		item.write(new File("/home/eduardo/Documentos/NetBeansProjects/djudge/" + item.getName()));
+//	    } catch (Exception ex) {
+//		Logger.getLogger(SubmissionServlet.class.getName()).log(Level.SEVERE, null, ex);
+//	    }
+//	}
 
-      try { 
-         // Parse the request to get file items.
-         List fileItems = upload.parseRequest(request);
+
+// salva apenas source
+	boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 	
-         // Process the uploaded file items
-         Iterator i = fileItems.iterator();
+	File uploadedFile = null;
 
-         out.println("<html>");
-         out.println("<head>");
-         out.println("<title>Servlet upload</title>");  
-         out.println("</head>");
-         out.println("<body>");
-   
-         while ( i.hasNext () ) {
-            FileItem fi = (FileItem)i.next();
-            if ( !fi.isFormField () ) {
-               // Get the uploaded file parameters
-               String fieldName = fi.getFieldName();
-               String fileName = fi.getName();
-               String contentType = fi.getContentType();
-               boolean isInMemory = fi.isInMemory();
-               long sizeInBytes = fi.getSize();
-            
-        
+	if (isMultipart) {
+	    FileItemFactory factory = new DiskFileItemFactory();
+	    ServletFileUpload upload = new ServletFileUpload(factory);
+
+	    try {
+		List items = upload.parseRequest(request);
+		Iterator iterator = items.iterator();
+		while (iterator.hasNext()) {
+		    FileItem item = (FileItem) iterator.next();
+
+		    if (!item.isFormField()) {
+			String fileName = item.getName();
+
+			File path = new File("/home/eduardo/Documentos/NetBeansProjects/djudge/");
+			if (!path.exists()) {
+			    boolean status = path.mkdirs();
+			}
+
+			uploadedFile = new File(path + "/" + fileName);
+			System.out.println(uploadedFile.getAbsolutePath());
+			item.write(uploadedFile);
+		    }
+		}
+	    } catch (FileUploadException e) {
+		e.printStackTrace();
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+	}
+
+	
+	String line = null;
+	
+	BufferedReader br = new BufferedReader(new FileReader(uploadedFile));
+	while (br.ready()) {
+	    line = br.readLine();
+	}
+	br.close();
+	
+	
+// Cria Submissão	
+	String url = "https://a53fdb4d.compilers.sphere-engine.com/api/v4/submissions?access_token=8bce219ca0c54390d6b832fa542470b2";
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost request2 = new HttpPost(url);
+
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        urlParameters.add(new BasicNameValuePair("source", line));
+        urlParameters.add(new BasicNameValuePair("compilerId", "4"));
+
+        request2.setEntity(new UrlEncodedFormEntity(urlParameters));
+        //	request.setHeader("Content-Type", "multipart/form-data");
+        request2.addHeader("User-Agent", USER_AGENT);
+
+        HttpResponse response2 = client.execute(request2);
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + request2.getEntity());
+        System.out.println("Response Code : " + response2.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response2.getEntity().getContent()));
+        StringBuffer result = new StringBuffer();
+        String line2 = "";
+        while ((line2 = rd.readLine()) != null) {
+            result.append(line2);
+        }
+	
+	System.out.println(result.toString());
+	System.out.println(result.substring(6, 14));
+	
+	String id = result.substring(6, 14);
+	
+	
+	
+// Retorna dados da submissão
+	String url2 = "https://a53fdb4d.compilers.sphere-engine.com/api/v4/submissions/" + id + "?access_token=8bce219ca0c54390d6b832fa542470b2";
+	
+	HttpClient client2 = HttpClientBuilder.create().build();
+	HttpGet request3 = new HttpGet(url2);
+
+	// add request header
+	request3.addHeader("User-Agent", USER_AGENT);
+	HttpResponse response3 = client.execute(request3);
+
+	BufferedReader rd2 = new BufferedReader(
+		new InputStreamReader(response3.getEntity().getContent()));
+
+	StringBuffer result2 = new StringBuffer();
+	String line3 = "";
+	while ((line3 = rd2.readLine()) != null) {
+	    System.out.println(result2.append(line3));
+	}
+
+
+
+// Retorna output da submissão
+	String url3 = "https://a53fdb4d.compilers.sphere-engine.com/api/v4/submissions/" + id + "/output?access_token=8bce219ca0c54390d6b832fa542470b2";
+
+	HttpClient client3 = HttpClientBuilder.create().build();
+	HttpGet request4 = new HttpGet(url3);
+
+	request2.addHeader("User-Agent", USER_AGENT);
+	HttpResponse response4 = client3.execute(request4);
+
+	BufferedReader rd3 = new BufferedReader(
+		new InputStreamReader(response4.getEntity().getContent()));
+
+	StringBuffer result3 = new StringBuffer();
+	String line4 = "";
+	while ((line4 = rd3.readLine()) != null) {
+	    System.out.println(result3.append(line4));
+	}
+	
+    }
+
+}
